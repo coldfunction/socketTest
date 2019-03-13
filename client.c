9e4a7a8a262f3e85bea3b3590465b9b5841cb0ca
coldfunction <coldfunction@gmail.com>
2019-03-13 14:19:47 +0800

    feat: Constant size transfer client->server
    Record transfer rate from client to server

---
+++ client.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+#define TOTAL_LEN (64*1024)
+#define TOTAL_DATA_SIZE (4*1024*1024)
+
+
+int main(int argc , char *argv[])
+{
+
+    //socket build
+    int sockfd = 0;
+	int len = TOTAL_LEN;
+    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+
+    if (sockfd == -1){
+        printf("Fail to create a socket.");
+    }
+
+    //socket connect
+
+    struct sockaddr_in info;
+    bzero(&info,sizeof(info));
+    info.sin_family = PF_INET;
+
+    //localhost test
+    info.sin_addr.s_addr = inet_addr("172.31.3.2");
+    info.sin_port = htons(8700);
+
+
+    int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
+    if(err==-1){
+        printf("Connection error");
+    }
+
+
+    //Send a message to server
+	//
+
+	char *buf;
+	buf = malloc(len);
+	int i;
+	for(i = 0; i < len; i++)
+		buf[i] = '@';
+
+//    char message[] = {"Hi there"};
+    char receiveMessage[100] = {};
+	int offset = 0;
+
+	struct  timeval  start;
+	struct  timeval  end;
+	unsigned long timer;
+	gettimeofday(&start,NULL);
+
+
+	for(i = 0; i < TOTAL_DATA_SIZE/TOTAL_LEN; i++) {
+
+		do {
+	    	int ret = send(sockfd,buf + offset, len,0);
+			offset += ret;
+	    	len = len - ret;
+			printf("send len = %d\n", ret);
+			printf("rest len = %d\n", len);
+
+		}while (len);
+		printf("i = %d times\n", i);
+		len = TOTAL_LEN;
+		offset = 0;
+	}
+
+	recv(sockfd,receiveMessage,sizeof(receiveMessage),0);
+
+	gettimeofday(&end,NULL);
+	timer = 1000000 * (end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec;
+
+
+    printf("%s",receiveMessage);
+    printf("close Socket\n");
+    printf("timer = %ld us\n",timer);
+
+	printf("average trans rate = %ld\n", TOTAL_DATA_SIZE/timer);
+
+	close(sockfd);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+#define TOTAL_LEN (64*1024)
+#define TOTAL_DATA_SIZE (4*1024*1024)
+
+int main(int argc , char *argv[])
+
+{
+    //socket build
+//    char inputBuffer[256] = {};
+	char *inputBuffer;
+	int len = TOTAL_LEN;
+	inputBuffer = malloc(len);
+
+    char message[] = {"Hi,this is server okokokok.\n"};
+    int sockfd = 0,forClientSockfd = 0;
+    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+
+    if (sockfd == -1){
+        printf("Fail to create a socket.");
+    }
+
+    //socket connect
+    struct sockaddr_in serverInfo,clientInfo;
+    int addrlen = sizeof(clientInfo);
+    bzero(&serverInfo,sizeof(serverInfo));
+
+    serverInfo.sin_family = PF_INET;
+    serverInfo.sin_addr.s_addr = inet_addr("172.31.3.2");
+    serverInfo.sin_port = htons(8700);
+    bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
+    listen(sockfd,5);
+
+	int total = TOTAL_DATA_SIZE;
+    while(1){
+        forClientSockfd = accept(sockfd,(struct sockaddr*) &clientInfo, &addrlen);
+
+		int ret, offset = 0;
+		while ( ret = recv(forClientSockfd,inputBuffer+offset,len,0) ) {
+			offset += ret;
+			len = len - ret;
+//			printf("cocotion recv len = %d\n", ret);
+			if(len == 0) {
+				total-=offset;
+				printf("cocotion test rest recv = %d\n", total);
+				if(total == 0)
+					break;
+				else {
+					offset = 0;
+					len = TOTAL_LEN;
+				}
+			}
+		}
+		printf("cocotion test ok I recv all\n");
+
+		int i;
+		//for(i = 0; i < TOTAL_LEN; i++) {
+		//	if(inputBuffer[i] != '@' ) break;
+		//}
+		//if(i != TOTAL_LEN) {
+		//	printf("i = %d\n", i);
+		//	message[0] = '@';
+		//}
+        send(forClientSockfd,message,sizeof(message),0);
+//        printf("Get:%s\n",inputBuffer);
+    }
+    return 0;
+}

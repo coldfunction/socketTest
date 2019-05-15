7e088488ab858fc4850ed18ce6e23be4cd3fd0fc
coldfunction <coldfunction@gmail.com>
2019-05-15 12:07:44 +0800

    test: 4 VMs ok

--- client.c
+++ client.c
@@ -581,8 +581,9 @@ void *trans_func(void *data)
 
 //0.5, 4
 
-		float k = 0.65;
-		if(local_op == 2) {
+//		float k = 0.9; //current 4 VMs good
+		float k = 0.896; //
+		if(local_op == 3) {
 //			printf("cocotion before sleep timer = %ld time_slice  = %d\n", timer, time_slice);
 			if(timer < k*time_slice) {
 				usleep(k*time_slice-timer);
